5f48ee339697b2461f2040b3c115b9e0c8dcac79
Peter Bell <bellpeterm@users.noreply.github.com>
2016-01-12 16:08:34 -0800

    comment out redundant c import

--- src/spt_status.c
+++ src/spt_status.c
@@ -55,7 +55,7 @@
 #endif
 #ifdef HAVE_SYS_PRCTL_H
 #include <sys/prctl.h>          /* for Linux >= 2.6.9 */
-#include <linux/prctl.h>
+/*#include <linux/prctl.h>         comment out for Alpine Linux 3.3 */
 #endif
 #if defined(__darwin__)
 #include <crt_externs.h>
